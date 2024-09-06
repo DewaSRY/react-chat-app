576ec6307ae713656339e231a9a419857e3b9b05
DewaSRY <sdewa6645@gmail.com>
2024-09-06 17:03:30 +0800

    init:setup shac ui and herror background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { cn } from "./lib/utils";
+
 function App() {
-  return <div className="w-[90vh] mx-auto"></div>;
+  return (
+    <div
+      className={cn(
+        "my-[10vh] mx-auto py-8 px-4",
+        "w-[90vh]   bg-gray-700/80 rounded-2xl "
+      )}
+    >
+      hallo
+    </div>
+  );
 }
 
 export default App;
